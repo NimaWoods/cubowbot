c9af26dae11b51ef2f5ab93b7fa73ab68cda4b81
NimaWoods <niklasfabritzius22@gmail.com>
2024-03-23 22:25:40 +0100

    Changed status.html

--- src/main/java/com/cubowbot/cubow/controller/StatusController.java
+++ src/main/java/com/cubowbot/cubow/controller/StatusController.java
@@ -2,9 +2,13 @@ package com.cubowbot.cubow.controller;
 
 import com.cubowbot.cubow.CubowApplication;
 import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.requests.Route;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class StatusController {
@@ -15,10 +19,13 @@ public class StatusController {
         CubowApplication cubowApplication = new CubowApplication();
         JDA bot = cubowApplication.getJDA();
 
+        List<Guild> guilds = bot.getGuilds();
+
         model.addAttribute("logo", bot.getSelfUser().getAvatarUrl());
 
         model.addAttribute("botName", bot.getSelfUser().getName());
         model.addAttribute("botStatus", bot.getStatus().name());
+        model.addAttribute("botGuilds", guilds.size());
 
         return "status";
     }
