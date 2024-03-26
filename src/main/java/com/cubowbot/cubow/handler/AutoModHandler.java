9d4c96c03bc6c62fd565e8031f38348dee577f22
Nima <niklasfabritzius22@gmail.com>
2024-03-26 08:53:41 +0100

    Added basic Automod

--- src/main/java/com/cubowbot/cubow/handler/AutoModHandler.java
+++ src/main/java/com/cubowbot/cubow/handler/AutoModHandler.java
@@ -2,8 +2,13 @@ package com.cubowbot.cubow.handler;
 
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.interactions.components.buttons.Button;
 
 import java.awt.Color;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,21 +16,53 @@ public class AutoModHandler {
 
     public void checkForBadWord(MessageReceivedEvent event) {
 
-        List<String> badWords = new ArrayList<String>();
-        badWords.add("卐");
-        badWords.add("卍");
+        List<String> badWordsList = new ArrayList<String>();
+        String urlEN = "https://raw.githubusercontent.com/LDNOOBW/List-of-Dirty-Naughty-Obscene-and-Otherwise-Bad-Words/master/en";
+        String urlDE = "https://raw.githubusercontent.com/LDNOOBW/List-of-Dirty-Naughty-Obscene-and-Otherwise-Bad-Words/master/de";
+
+        badWordsList.add("卐");
+        badWordsList.add("卍");
+
+
+        // Loading BadWords list
+        try {
+            // English
+            URL fileUrlEN = new URL(urlEN);
+            BufferedReader readerEN = new BufferedReader(new InputStreamReader(fileUrlEN.openStream()));
+
+            String lineEN;
+            while ((lineEN = readerEN.readLine()) != null) {
+                badWordsList.add(lineEN);
+            }
+            readerEN.close();
+
+            // Deutsch
+            URL fileUrlDE = new URL(urlDE);
+            BufferedReader readerDE = new BufferedReader(new InputStreamReader(fileUrlDE.openStream()));
+
+            String lineDE;
+            while ((lineDE = readerDE.readLine()) != null) {
+                badWordsList.add(lineDE);
+            }
+            readerDE.close();
+        } catch (IOException e) {
+            System.err.println("Error reading from URL: " + e.getMessage());
+            e.printStackTrace();
+        }
 
         String content = event.getMessage().getContentDisplay();
-        for (String word : badWords) {
+        for (String word : badWordsList) {
             if (content.contains(word)) {
 
                 EmbedBuilder embedBuilder = new EmbedBuilder();
                 embedBuilder.setTitle("Bad Word");
-                embedBuilder.setDescription("This Word or Symbol is not allowed on this server");
-                embedBuilder.addField("Word detected", word, true);
+                embedBuilder.setDescription("Your message contains a Word that is not allowed on this server");
+                embedBuilder.addField("User", event.getMember().getAsMention(), true);
                 embedBuilder.setColor(Color.MAGENTA);
 
-                event.getMessage().replyEmbeds(embedBuilder.build()).queue();
+                event.getMessage().replyEmbeds(embedBuilder.build())
+                        .addActionRow(Button.link("https://github.com/LDNOOBW/List-of-Dirty-Naughty-Obscene-and-Otherwise-Bad-Words/tree/master", "Bad Word List"))
+                        .queue();
                 event.getMessage().delete().queue();
                 return;
             }
