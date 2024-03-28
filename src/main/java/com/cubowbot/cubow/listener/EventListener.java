e759a55f429c90efd8fd3b41bac305b40a77130c
NimaWoods <niklasfabritzius22@gmail.com>
2024-03-28 21:41:33 +0100

    Changed onjoin handler

--- src/main/java/com/cubowbot/cubow/listener/EventListener.java
+++ src/main/java/com/cubowbot/cubow/listener/EventListener.java
@@ -70,10 +70,14 @@ public class EventListener extends ListenerAdapter {
     @Override
     public void onGuildJoin(GuildJoinEvent event) {
         EmbedBuilder embedBuilder = new EmbedBuilder();
-        embedBuilder.setTitle("Hallo Cubow");
+        embedBuilder.setTitle("Hallo, ich bin Cubow");
         embedBuilder.setDescription("Vielen Dank, dass du Cubow nutzt. Wir sind gerade noch mitten im Aufbau, aber wir freuen uns, dass du dabei bist.");
-        embedBuilder.addField("Website", "cubow.nimawoods.de", true);
-        event.getGuild().getDefaultChannel().asTextChannel().sendMessageEmbeds(embedBuilder.build()).queue();
+        embedBuilder.addField("Cubow einrichten", "/setup", true);
+        embedBuilder.setColor(Color.MAGENTA);
+        event.getGuild().getDefaultChannel().asTextChannel().sendMessageEmbeds(embedBuilder.build())
+                .setActionRow(Button.link("https://discord.com/oauth2/authorize?client_id=1217485873508253839", "Cubow einladen"),
+                        Button.link("https://discord.gg/xHYD4Bm5x6", "Cubow Discord Server"))
+                .queue();
     }
 
     @Override
