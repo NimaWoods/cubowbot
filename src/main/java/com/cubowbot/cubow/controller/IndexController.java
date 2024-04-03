1b53360a9053ca2a80f1418d759935344b3e1351
NimaWoods <niklasfabritzius22@gmail.com>
2024-04-03 23:18:28 +0200

    New Website Design

--- src/main/java/com/cubowbot/cubow/controller/IndexController.java
+++ src/main/java/com/cubowbot/cubow/controller/IndexController.java
@@ -2,6 +2,7 @@ package com.cubowbot.cubow.controller;
 
 import com.cubowbot.cubow.CubowApplication;
 import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.interactions.commands.Command;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,12 +16,25 @@ public class IndexController {
         CubowApplication cubowApplication = new CubowApplication();
         JDA bot = cubowApplication.getJDA();
 
-        if(bot.getStatus().name().equals("CONNECTED")) {
-            model.addAttribute("botStatus", "Online");
-        } else {
-            model.addAttribute("botStatus", bot.getStatus().name());
+        long guilds = bot.getGuilds().stream().count();
+        long totalUsers = bot.getGuilds().stream()
+                .mapToLong(guild -> guild.getMemberCount())
+                .sum();
+
+        long count = 0;
+        for (Command command : bot.retrieveCommands().complete()) {
+            count++;
         }
 
-        return "comingsoon";
+
+        String logoURL = bot.getSelfUser().getAvatarUrl();
+        logoURL = logoURL + "?size=512";
+
+        model.addAttribute("logo", logoURL);
+        model.addAttribute("guilds", guilds);
+        model.addAttribute("commands", count);
+        model.addAttribute("user", totalUsers);
+
+        return "index";
     }
 }
